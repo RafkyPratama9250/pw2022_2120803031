65b5b00f874988c1e15a04278524cfbc01b10792
rafky pratama <razakiandromaxa@gmail.com>
2022-11-09 11:07:41 +0700

    ini adalah pertemuan 9

---
+++ Pertemuan_9/strlen.php
@@ -0,0 +1,8 @@
+<?php
+$text1 = "selamat datang di kuliah pemerogaman web 2";
+$text2 = "Universitas pasundan";
+$text3 = "sandikagalih";
+
+echo strlen($text1) . "<br>";
+echo strlen($text2) . "<br>";
+echo strlen($text3) . "<br>";
