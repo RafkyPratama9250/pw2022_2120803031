fb7ea748dcf42b7bbdde98ef8f3105367af5f083
Rafky Pratama <razakiandromaxa@gmail.com>
2022-11-26 22:34:34 +0700

    ini adalah tugas pertemuan 11

--- Pertemuan_11/test.php
+++ Pertemuan_11/test.php
@@ -1,6 +1,37 @@
-<?php
-$name = $_GET['name'];
-$age = $_GET['age'];
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
 
-echo $name;
-echo $age;
+<!DOCTYPE html>
+
+<head>
+     
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+     <title>Belajar Form PHP</title>
+</head>
+
+<body>
+     <h2>Tutorial Belajar Form HTML - PHP </h2>
+     <form action="proses.php" method="get">
+         Nama: <input type="text" name="nama" />
+         <br />
+         E-Mail: <input type="text" name="email" />
+         <br />
+         <input type="submit" value="Proses Data">
+       </form>
+</body>
+
+</html>
